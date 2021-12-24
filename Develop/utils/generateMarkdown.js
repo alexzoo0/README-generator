59d6f5a370f6a5389409ef652aca800d1bf302dc
alexzoo0 <alexisvelasquez15@gmail.com>
2021-12-24 11:30:45 -0600

    added functionality to create read me

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -4,15 +4,47 @@ const inquirer = require('inquirer');
 const index = require('../index.js');
 
 // If there is no license, return an empty string
-function renderLicenseBadge(license){}
+function renderLicenseBadge(license){
+  const badge = '';
+  if (license === 'MIT'){
+    badge = '![Github License](https://img.shields.io/github/license/Naereen/StrapDown.js.svg)'
+  }else if (license === 'Apache 2.0') {
+    badge = '![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)'
+  }else if (license === 'GPL v3.0') {
+    badge = '![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)'
+  }else{
+    badge = ''
+  }
+  return badge;
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  const licenses
+  if(license === 'MIT'){
+    licenses = 'https://choosealicense.com/licenses/mit/'
+  }else if (license === 'Apache 2.0'){
+    licenses = 'http://www.apache.org/licenses/LICENSE-2.0'
+  }else if (license === 'GPL v3.0'){
+    licenses = 'https://www.gnu.org/licenses'
+  }else{
+    licenses = ''
+  }
+  return licenses;
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  const section = ''
+  if (license === 'None'){
+    section = ''
+  } else{
+    section = `license: ${license}`
+  }
+  return section;
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
